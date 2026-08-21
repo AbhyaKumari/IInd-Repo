a08dfff1010e25681472d8682d19e3ca0704ec11
Abhya Kumari <abhyakumari992026@gmail.com>
2026-08-21 19:30:28 +0530

    code redefined

--- sum.py
+++ sum.py
@@ -1,9 +1,7 @@
 # Storing numbers in variables
 num1 = 15
 num2 = 12
-
 # Calculating the sum
 total = num1 + num2
-
 # Printing the result using an f-string
 print("The sum of both nos. is", total)
